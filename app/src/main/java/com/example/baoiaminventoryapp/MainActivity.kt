c5d3ce68e5fc8fbfba156a7d4ea6b8c0018bc0de
Jatin Kumar <jatinkumar8125@gmail.com>
2024-07-09 15:57:36 +0530

    initial commit

--- app/src/main/java/com/example/baoiaminventoryapp/MainActivity.kt
+++ app/src/main/java/com/example/baoiaminventoryapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.baoiaminventoryapp.screens.LoginPage
 import com.example.baoiaminventoryapp.ui.theme.BaoiamInventoryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BaoiamInventoryAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoginPage()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BaoiamInventoryAppTheme {
-        Greeting("Android")
     }
 }
