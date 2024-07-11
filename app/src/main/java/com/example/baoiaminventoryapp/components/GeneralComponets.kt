2d1c71075a68d4356a3459bf28b5958d089da049
Jatin Kumar <jatinkumar8125@gmail.com>
2024-07-12 04:36:51 +0530

    UI changes and code declutter

--- app/src/main/java/com/example/baoiaminventoryapp/components/GeneralComponets.kt
+++ app/src/main/java/com/example/baoiaminventoryapp/components/GeneralComponets.kt
@@ -3,20 +3,27 @@ package com.example.baoiaminventoryapp.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun Spacing(height: Int) {
-    Spacer(modifier = Modifier.height(height.dp))
+fun Spacing(height: Int = 0, width: Int = 0) {
+    Spacer(modifier = Modifier.height(height.dp).width(width.dp))
 }
 
 @Composable
@@ -30,4 +37,30 @@ fun EnclosingBox (content:@Composable () -> Unit) {
     ){
         content()
     }
+}
+
+@Composable
+fun SubmitButton () {
+    Button(onClick = { /*TODO*/ },
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 40.dp)
+            .height(50.dp)
+    ){
+        Text(text = "Submit", fontSize = 16.sp)
+    }
+}
+
+@Composable
+fun HeaderText (qrCodeDetected: Boolean = false, modifier:Modifier) {
+    val headerText = if (qrCodeDetected) "No barcode detected" else "Barcode infromation"
+    Text(
+        text = headerText,
+        fontSize = 25.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Monospace,
+        modifier = modifier
+    )
 }
--- app/src/main/java/com/example/baoiaminventoryapp/screens/HomePage.kt
+++ app/src/main/java/com/example/baoiaminventoryapp/screens/HomePage.kt
@@ -1,28 +1,22 @@
 package com.example.baoiaminventoryapp.screens
 
-
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +26,9 @@ import com.example.baoiaminventoryapp.components.CustomButton
 import com.example.baoiaminventoryapp.components.CustomTextField
 import com.example.baoiaminventoryapp.components.DivueensLogo
 import com.example.baoiaminventoryapp.components.EnclosingBox
+import com.example.baoiaminventoryapp.components.HeaderText
 import com.example.baoiaminventoryapp.components.Spacing
+import com.example.baoiaminventoryapp.components.SubmitButton
 
 @Composable
 fun HomePage(dataOfQR: String) {
@@ -54,15 +50,13 @@ fun HomePage(dataOfQR: String) {
                 CustomButton(
                     label = "Log out",
                     color = Color.Black,
-                    height = 40,
-                    width = 120,
+                    height = 40, width = 120,
                     onClick = {}
                 )
                 CustomButton(
                     label = "Start Scanning",
                     color = colorPallete,
-                    height = 40,
-                    width = 160,
+                    height = 40, width = 160,
                     onClick = {}
                 )
             }
@@ -73,17 +67,9 @@ fun HomePage(dataOfQR: String) {
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(start = 15.dp)
                 ) {
                     Spacing(height = 30)
-                    val headerText = if (true /* QR code detected*/) "No barcode detected" else "Barcode infromation"
-                    Text(
-                        text = headerText,
-                        fontSize = 25.sp,
-                        fontWeight = FontWeight.Bold,
-                        fontFamily = FontFamily.Monospace,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    )
+                    HeaderText(modifier = Modifier.align(Alignment.CenterHorizontally)) // I dont like this at all I will replace/ make it look better later
                     Spacing(height = 20)
                     scannedLines.forEach { line ->
                         val colonSeperatedLines = line.split(":")
@@ -92,17 +78,18 @@ fun HomePage(dataOfQR: String) {
                                 text = colonSeperatedLines[0] + ": ",
                                 fontWeight = FontWeight.Bold,
                                 fontSize = 25.sp,
-                                modifier = Modifier.padding(vertical = 4.dp)
+                                modifier = Modifier.padding(start = 15.dp, bottom = 8.dp)
                             )
                             Text(
                                 text = colonSeperatedLines[1],
                                 fontSize = 25.sp,
-                                modifier = Modifier.padding(vertical = 4.dp)
+                                modifier = Modifier.padding(bottom = 8.dp)
                             )
                         }
                     }
                     Spacing(height = 5)
                     Row(verticalAlignment = Alignment.CenterVertically) {
+                        Spacing(width = 15)
                         Text(
                             text = "Aisle no.: ",
                             fontWeight = FontWeight.Bold,
@@ -114,14 +101,8 @@ fun HomePage(dataOfQR: String) {
                                 .width(150.dp)
                         )
                     }
-                    Spacing(height = 20)
-                    CustomButton(
-                        label = "Submit",
-                        color = Color.Black,
-                        height = 50,
-                        width = 200,
-                        onClick = {}
-                    )
+                    Spacing(height = 30)
+                    SubmitButton()
                 }
             }
         }
